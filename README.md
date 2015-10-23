92d18b13e419edd3998eb96ad53670ebdd032cce
Branden Cash <bcash@parchment.com>
2015-10-23 16:57:17 -0700

    Switch to DAV hosted svn server

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 Dockerfile for Subversion
 ===
 
-The simplest subversion server by `svnserve`.
+SVN Server supported by apache
 
 ## Usage with boot2docker
 
 ```sh
 docker pull mashupmill/subversion
-docker run -d --name=svn-repo -p 3690:3690 \
-    --env COMPATIBLE_VERSION=1.6 --env ANON_ACCESS=none \
+docker run -d --name=svn-repo -p 80:80 \
+    --env COMPATIBLE_VERSION=1.6 \
     --env USERNAME=my-user --env PASSWORD=my-generated-password \
     mashupmill/subversion
 cd your/work/dir
-svn co svn://$(boot2docker ip)/ repo
+svn co http://$(boot2docker ip)/ repo
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,23 +2,17 @@
 
 if [ ! "`ls -A /data`" ]; then
     echo -n Creating SVN Server
+    mkdir /data/repos
+    htpasswd -b -c /data/.htpasswd $USERNAME $PASSWORD
+    echo '[/]' >> /data/authz
+    echo '* = rw' >> /data/authz
     [ "$COMPATIBLE_VERSION" != "" ] && CREATE_OPTS="$CREATE_OPTS --compatible-version $COMPATIBLE_VERSION"
     echo " with options \"$CREATE_OPTS\""
-    svnadmin create $CREATE_OPTS /data
-    sed -E -i "s/(# )?anon-access.*/anon-access = $ANON_ACCESS/" "/data/conf/svnserve.conf"
-    sed -E -i "s/(# )?auth-access.*/auth-access = $AUTH_ACCESS/" "/data/conf/svnserve.conf"
-    sed -E -i "s/(# )?realm.*/realm = svnrealm/" "/data/conf/svnserve.conf"
-    sed -E -i "s/(# )?use-sasl.*/use-sasl = true/" "/data/conf/svnserve.conf"
-    sed -E -i "s/(# )?min-encryption.*/min-encryption = 128/" "/data/conf/svnserve.conf"
-    sed -E -i "s/(# )?max-encryption.*/max-encryption = 256/" "/data/conf/svnserve.conf"
-
-    echo "$PASSWORD" | saslpasswd2 -p -c -f /data/conf/sasldb -u svnrealm $USERNAME
-    echo 'pwcheck_method: auxprop' >> /usr/lib/sasl2/svn.conf
-    echo 'auxprop_plugin: sasldb' >> /usr/lib/sasl2/svn.conf
-    echo 'sasldb_path: /data/conf/sasldb' >> /usr/lib/sasl2/svn.conf
-    echo 'mech_list: DIGEST-MD5' >> /usr/lib/sasl2/svn.conf
+    svnadmin create $CREATE_OPTS /data/repos
+    chown -R www-data:www-data /data/repos
 fi
 
 echo Starting SVN Server
 
-svnserve --foreground -d -r /data
+#svnserve --foreground -d -r /data
+/usr/sbin/apache2ctl -D FOREGROUND
